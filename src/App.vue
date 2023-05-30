4bebab5ba0ebf9f780673f8395fa388b0bc8b79c
isishome <isishome@hanmail.net>
2023-05-31 00:05:08 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,7 @@ export default {
   preFetch({ store }) {
     const as = useAccountStore(store)
     const is = useItemStore(store)
-    //return Promise.all([as.checkSign(), as.getEvaluations(), is.getBase(), is.getProperties(), is.getAffixes(), is.getRestrictions()])
-    return Promise.all([as.getEvaluations(), is.getBase(), is.getProperties(), is.getAffixes(), is.getRestrictions()])
+    return Promise.all([as.checkSign(), as.getEvaluations(), is.getBase(), is.getProperties(), is.getAffixes(), is.getRestrictions()])
   }
 }
 </script>
