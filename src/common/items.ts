19c13b322b19400a9ebebfea6f1ef391b0f01cb0
isishome <isishome@hanmail.net>
2023-06-12 23:55:50 +0900

    add globe image

--- src/common/items.ts
+++ src/common/items.ts
@@ -2,7 +2,7 @@ export const itemImgs: { [key: string]: any } = {
   weapon: {
     axe: 24, twohandedaxe: 17, bow: 19, crossbow: 19, dagger: 29, focus: 23, mace: 24, twohandedmace: 24, polearm: 18, scythe: 14, twohandedscythe: 18, staff: 46, sword: 26, twohandedsword: 19, wand: 26
   },
-  armor: { boots: 64, shield: 20, chestarmor: 66, gloves: 66, helm: 68, pants: 65 },
+  armor: { boots: 64, shield: 20, chestarmor: 66, gloves: 67, helm: 68, pants: 65 },
   accesesory: { amulet: 39, ring: 61 },
   inventory: { totem: 21 }
 }
