2710ac66defe4df720d603f60b2b8e42e6d554cf
isishome <isishome@hanmail.net>
2023-06-22 15:33:17 +0900

    update battletag regexp

--- src/App.vue
+++ src/App.vue
@@ -96,8 +96,8 @@ onMounted(() => {
         <div class="q-px-lg q-py-md">
           <q-input autofocus v-model="battleTag" label="BattleTag™" :placeholder="t('battlenet.placeholder')" fill-mask
             :disable="loading"
-            :rules="[val => val && (/^([ㄱ-ㅎㅏ-ㅣ가-힣]{1}[ㄱ-ㅎㅏ-ㅣ가-힣0-9]{1,7}#[0-9]{4,6}|[a-zA-Z]{1}[a-zA-Z0-9]{2,11}#[0-9]{4,6})$/g).test(val) || '']"
-            outlined no-error-icon hide-hint maxlength="17" />
+            :rules="[val => val && (/^([ㄱ-ㅎㅏ-ㅣ가-힣]{1}[ㄱ-ㅎㅏ-ㅣ가-힣0-9]{1,7}#[0-9]{4,}|[a-zA-Z]{1}[a-zA-Z0-9]{2,11}#[0-9]{4,})$/g).test(val) || '']"
+            outlined no-error-icon hide-hint maxlength="24" />
         </div>
       </q-card-section>
     </template>
