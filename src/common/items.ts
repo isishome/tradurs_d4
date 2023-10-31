dfbffcc70819ae2f7d5d13f17b3204dfe78c3ae1
isishome <isishome@hanmail.net>
2023-10-31 10:38:27 +0900

    add image

--- src/common/items.ts
+++ src/common/items.ts
@@ -2,6 +2,6 @@ export const itemImgs: { [key: string]: any } = {
   weapon: {
     axe: 24, twohandedaxe: 17, bow: 19, crossbow: 19, dagger: 29, focus: 23, mace: 24, twohandedmace: 24, polearm: 18, scythe: 14, twohandedscythe: 18, staff: 46, sword: 26, twohandedsword: 19, totem: 21, wand: 26, shield: 20
   },
-  armor: { boots: 64, chestarmor: 67, gloves: 70, helm: 68, pants: 65 },
-  accessory: { amulet: 39, ring: 61 }
+  armor: { boots: 85, chestarmor: 67, gloves: 70, helm: 68, pants: 65 },
+  accessory: { amulet: 41, ring: 61 }
 }
