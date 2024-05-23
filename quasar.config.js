7b711ce9ecee1c083f12a08097c0336b0767c709
isishome <isishome@hanmail.net>
2024-05-23 17:15:28 +0900

    node 20 and fixed bug

--- quasar.config.js
+++ quasar.config.js
@@ -13,6 +13,7 @@ const { configure } = require('quasar/wrappers');
 const path = require('path');
 //const { mergeConfig } = require('vite')
 
+
 module.exports = configure(function (/* ctx */) {
   return {
     // https://v2.quasar.dev/quasar-cli-vite/prefetch-feature
