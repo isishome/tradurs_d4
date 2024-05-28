0cada943ac95d89570546a654bb0b9def93c317e
isishome <isishome@hanmail.net>
2024-05-29 01:47:55 +0900

    shrink docker image

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
-# alpine 20 버전 기반
-FROM node:20-alpine
+# alpine 20 버전 기반 빌더
+FROM node:20-alpine AS builder
 
 # 작업 디렉토리
 WORKDIR /d4-proj
@@ -11,15 +11,17 @@ COPY . .
 RUN npm install \
     && npx quasar build -m ssr
 
-# ssr 의존성 설치 및 빌드 정리
+# ssr 의존성 설치
 WORKDIR /d4-proj/dist/ssr
-RUN npm install \
-    && mkdir -p /d4 \
-    && cp -r * /d4 \
-    && rm -rf /d4-proj
+RUN npm install 
 
-# 서버 구동 준비
+# alpine 20 버전 기반
+FROM node:20-alpine
+
+# 작업 디렉토리
 WORKDIR /d4
+
+COPY --from=builder /d4-proj/dist/ssr /d4
 
 # 포트 명시
 EXPOSE 6090
--- src/pages/admin/UserPage.vue
+++ src/pages/admin/UserPage.vue
@@ -1,28 +1,42 @@
 <script setup lang="ts">
-import { ref, reactive, computed, watch, onMounted } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
-import { useQuasar } from 'quasar'
-import { useI18n } from 'vue-i18n'
-import { type IUser, useAdminStore } from 'stores/admin-store'
-import { clipboard } from 'src/common'
+import { ref, reactive, computed, watch, onMounted } from "vue"
+import { useRoute, useRouter } from "vue-router"
+import { useQuasar } from "quasar"
+import { useI18n } from "vue-i18n"
+import { type IUser, useAdminStore } from "stores/admin-store"
+import { clipboard } from "src/common"
 
 const route = useRoute()
 const router = useRouter()
 const $q = useQuasar()
 const as = useAdminStore()
-const { t } = useI18n({ useScope: 'global' })
+const { t } = useI18n({ useScope: "global" })
 
 const disable = ref<boolean>(true)
 const selectAll = ref<boolean>(false)
-const page = ref<number>(route.query.page ? parseInt(route.query.page as string) : 1)
-const status = computed(() => (state: string) => state === '000' ? '신규(미 인증)' : state === '001' ? '활성' : '비 활성')
-const colors = computed(() => (state: string) => state === '000' ? 'yellow-9' : state === '001' ? 'positive' : 'negative')
+const page = ref<number>(
+  route.query.page ? parseInt(route.query.page as string) : 1
+)
+const status = computed(
+  () => (state: string) =>
+    state === "000" ? "신규(미 인증)" : state === "001" ? "활성" : "비 활성"
+)
+const colors = computed(
+  () => (state: string) =>
+    state === "000" ? "yellow-9" : state === "001" ? "positive" : "negative"
+)
 const users = reactive<Array<IUser>>([])
-const selected = computed(() => users.filter(u => u.selected).map(u => u.identity))
+const selected = computed(() =>
+  users.filter((u) => u.selected).map((u) => u.identity)
+)
 
 const getUsers = async (p?: number) => {
   if (!!p && p !== page.value)
-    return router.push({ name: 'adminUser', params: { lang: route.params.lang }, query: { page: p } })
+    return router.push({
+      name: "adminUser",
+      params: { lang: route.params.lang },
+      query: { page: p }
+    })
 
   disable.value = true
   users.splice(0, users.length)
@@ -32,30 +46,43 @@ const getUsers = async (p?: number) => {
 }
 
 const move = (val: number) => {
-  router.push({ name: 'adminUser', params: { lang: route.params.lang }, query: { page: page.value + val } })
+  router.push({
+    name: "adminUser",
+    params: { lang: route.params.lang },
+    query: { page: page.value + val }
+  })
 }
 
 const searchInfo = ref<string>()
 const sendVerifyEmail = (identity: string) => {
   disable.value = true
-  as.resendVerify(identity).then(() => {
-    $q.notify({ message: '인증 메일이 발송이 완료되었습니다.', color: 'positive', classes: '' })
-  }).catch(() => { }).then(() => {
-    disable.value = false
-  })
+  as.resendVerify(identity)
+    .then(() => {
+      $q.notify({
+        message: "인증 메일이 발송이 완료되었습니다.",
+        color: "positive",
+        classes: ""
+      })
+    })
+    .catch(() => {})
+    .then(() => {
+      disable.value = false
+    })
 }
 
-watch(() => route.query.page, async (val, old) => {
-  if (val !== old && route.name === 'adminUser') {
-    page.value = val ? parseInt(val as string) : 1
-    await Promise.all([getUsers()])
+watch(
+  () => route.query.page,
+  async (val, old) => {
+    if (val !== old && route.name === "adminUser") {
+      page.value = val ? parseInt(val as string) : 1
+      await Promise.all([getUsers()])
+    }
   }
-})
+)
 
 onMounted(async () => {
   await Promise.all([getUsers()])
 })
-
 </script>
 <template>
   <div>
@@ -63,21 +90,26 @@ onMounted(async () => {
       <div></div>
       <div class="row justify-end items-center q-gutter-x-sm">
         <q-input dense outlined v-model="searchInfo" />
-        <q-btn unelevated dense color="primary" label="검색" @click="getUsers(1)" />
+        <q-btn
+          unelevated
+          dense
+          color="primary"
+          label="검색"
+          @click="getUsers(1)"
+        />
       </div>
     </div>
     <q-markup-table flat bordered class="users overflow-hidden">
-      <colgroup>
-        <col style="width:48px" />
-        <col />
-        <col />
-        <col />
-        <col style="width:48px" />
-      </colgroup>
       <thead>
         <tr>
           <th>
-            <q-checkbox v-model="selectAll" @update:model-value="(val) => users.forEach(u => u.selected = val)" dense />
+            <q-checkbox
+              v-model="selectAll"
+              @update:model-value="
+                (val) => users.forEach((u) => (u.selected = val))
+              "
+              dense
+            />
           </th>
           <th>이메일</th>
           <th>배틀 태그</th>
@@ -88,22 +120,42 @@ onMounted(async () => {
       <tbody>
         <tr v-for="user in users" :key="user.identity">
           <td><q-checkbox v-model="user.selected" dense /></td>
-          <td class="ellipsis cursor-pointer" style="max-width:50px">
-            <span class="cursor-pointer underline" @click="clipboard(user.identity, '사용자 이메일')">
+          <td class="ellipsis cursor-pointer" style="max-width: 50px">
+            <span
+              class="cursor-pointer underline"
+              @click="clipboard(user.email, '사용자 이메일')"
+            >
               {{ user.email }}
             </span>
           </td>
-          <td class="ellipsis cursor-pointer" style="max-width:50px">
-            <span class="cursor-pointer underline" @click="clipboard(user.battleTag, '사용자 배틀 태그')">
+          <td class="ellipsis cursor-pointer" style="max-width: 50px">
+            <span
+              class="cursor-pointer underline"
+              @click="clipboard(user.battleTag, '사용자 배틀 태그')"
+            >
               {{ user.battleTag }}
             </span>
           </td>
-          <td :class="`text-${colors(user.status)}`">{{ status(user.status) }}</td>
+          <td :class="`text-${colors(user.status)}`">
+            {{ status(user.status) }}
+          </td>
           <td>
-            <q-btn-dropdown color="primary" label="기능" dense unelevated content-class="bordered" class="no-hover"
-              :ripple="false" dropdown-icon="img:/images/icons/dropdown.svg">
+            <q-btn-dropdown
+              color="primary"
+              label="기능"
+              dense
+              unelevated
+              content-class="bordered"
+              class="no-hover"
+              :ripple="false"
+              dropdown-icon="img:/images/icons/dropdown.svg"
+            >
               <q-list dense>
-                <q-item clickable v-close-popup @click="sendVerifyEmail(user.identity)">
+                <q-item
+                  clickable
+                  v-close-popup
+                  @click="sendVerifyEmail(user.identity)"
+                >
                   <q-item-section>
                     <q-item-label>인증 메일 재 전송</q-item-label>
                   </q-item-section>
@@ -123,15 +175,43 @@ onMounted(async () => {
       </q-inner-loading>
     </q-markup-table>
     <div class="row justify-between q-mt-md q-px-sm paging">
-      <div>{{ t('message.page', { page }) }}</div>
+      <div>{{ t("message.page", { page }) }}</div>
       <div class="row justify-end items-center q-gutter-x-md">
-        <q-btn flat dense round padding="0" aria-label="Tradurs Prev Button" :disable="!as.over || disable"
-          :shadow="!$q.dark.isActive" @click="move(-1)">
-          <img src="/images/icons/prev.svg" width="24" height="24" class="icon" alt="Tradurs Prev Icon" />
+        <q-btn
+          flat
+          dense
+          round
+          padding="0"
+          aria-label="Tradurs Prev Button"
+          :disable="!as.over || disable"
+          :shadow="!$q.dark.isActive"
+          @click="move(-1)"
+        >
+          <img
+            src="/images/icons/prev.svg"
+            width="24"
+            height="24"
+            class="icon"
+            alt="Tradurs Prev Icon"
+          />
         </q-btn>
-        <q-btn flat dense round padding="0" aria-label="Tradurs Next Button" :disable="!as.more || disable"
-          :shadow="!$q.dark.isActive" @click="move(1)">
-          <img src="/images/icons/next.svg" width="24" height="24" class="icon" alt="Tradurs Next Icon" />
+        <q-btn
+          flat
+          dense
+          round
+          padding="0"
+          aria-label="Tradurs Next Button"
+          :disable="!as.more || disable"
+          :shadow="!$q.dark.isActive"
+          @click="move(1)"
+        >
+          <img
+            src="/images/icons/next.svg"
+            width="24"
+            height="24"
+            class="icon"
+            alt="Tradurs Next Icon"
+          />
         </q-btn>
       </div>
     </div>
