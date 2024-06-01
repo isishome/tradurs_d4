9429e4f662351af5f49c0d2b3b4140ec37175b81
isishome <isishome@hanmail.net>
2024-06-01 22:19:26 +0900

    fixed css bug

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -287,12 +287,11 @@ const getList = async (scrollTop?: boolean) => {
   rewardItem.value = undefined
 
   if (is.showRewardItem) {
-    // const awardsPick = is.awardsPick.filter(
-    //   (ap: AwardsPick) =>
-    //     ap.hardcore === is.storage.data.hardcore &&
-    //     ap.ladder === is.storage.data.ladder
-    // )
-    const awardsPick = [{ itemId: 263 }]
+    const awardsPick = is.awardsPick.filter(
+      (ap: AwardsPick) =>
+        ap.hardcore === is.storage.data.hardcore &&
+        ap.ladder === is.storage.data.ladder
+    )
     if (awardsPick.length > 0) {
       const pickItemId =
         awardsPick[
