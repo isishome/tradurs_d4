f0b811d020e0e737856782020d2b205d8a527440
serasomething <isishome@hanmail.net>
2026-03-11 22:33:00 +0900

    update season 12

--- src/i18n/en/index.ts
+++ src/i18n/en/index.ts
@@ -78,7 +78,7 @@ export default {
     message: 'Do you want to delete this preset?'
   },
   season: {
-    bg: '/images/season/011/season_emblem_en.webp',
+    bg: '/images/season/012/season_emblem_en.webp',
     first: {
       socket:
         'During Season of the Malignant, the more accurately you select the socket type in the accessory item affixes, the more often your items will be seen'
@@ -246,7 +246,7 @@ export default {
   item: {
     hardcore: 'Hardcore',
     softcore: 'Softcore',
-    ladder: 'Season of DEVINE INTERVENTION',
+    ladder: 'Season of DEVINE SLAUGHTER',
     seasonal: 'Seasonal',
     eternal: 'Eternal',
     quality: 'Item Quality',
